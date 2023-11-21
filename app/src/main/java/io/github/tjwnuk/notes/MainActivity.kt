61b46b3945bba8fa00d2abce123db4751cd6de44
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-11-21 01:50:40 +0100

    yellow appBar

--- app/src/main/java/io/github/tjwnuk/notes/MainActivity.kt
+++ app/src/main/java/io/github/tjwnuk/notes/MainActivity.kt
@@ -1,28 +1,48 @@
 package io.github.tjwnuk.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import io.github.tjwnuk.notes.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold (
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text(text = "Notes") },
+                        colors = TopAppBarDefaults.mediumTopAppBarColors(
+                            containerColor = colorResource(id = R.color.yellow)
+                        ))
+                    }
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
